922b59305903e93a7cc72630bc10c187ef7c6341
MaccKOT <no@mail.com>
2024-06-11 01:25:47 +0400

    Cheerio installed

--- parseGW2TP.js
+++ parseGW2TP.js
@@ -3,9 +3,12 @@ import { Readable } from "node:stream";
 import { finished } from "node:stream/promises";
 
 // download gw2tp site whole source code
-const stream = fs.createWriteStream("gw2tp.txt");
-const { body } = await fetch("https://www.gw2tp.com/gems");
-await finished(Readable.fromWeb(body).pipe(stream));
+// const stream = fs.createWriteStream("gw2tp.txt");
+// const { body } = await fetch("https://www.gw2tp.com/gems");
+// await finished(Readable.fromWeb(body).pipe(stream));
+
+// we dont need 4 mb file
+// we can scrap prices from main page inside span.formatgw2money
 
 // find data in txt file or in stream: var data = [ ...... ] and extract last element []
 
