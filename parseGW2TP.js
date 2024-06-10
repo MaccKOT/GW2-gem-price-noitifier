e433756325544bdfaecc473b786f4360a4c15bd3
MaccKOT <no@mail.com>
2024-06-11 02:44:20 +0400

    Working parsing

--- parseGW2TP.js
+++ parseGW2TP.js
@@ -1,15 +1,26 @@
-import fs from "node:fs";
-import { Readable } from "node:stream";
-import { finished } from "node:stream/promises";
-
-// download gw2tp site whole source code
-// const stream = fs.createWriteStream("gw2tp.txt");
-// const { body } = await fetch("https://www.gw2tp.com/gems");
-// await finished(Readable.fromWeb(body).pipe(stream));
+import * as cheerio from "cheerio";
+import axios from "axios";
 
 // we dont need 4 mb file
-// we can scrap prices from main page inside span.formatgw2money
+// we can scrap current price from main page https://www.gw2tp.com/ -> inside span.gold and span.silver for 100 gems
 
-// find data in txt file or in stream: var data = [ ...... ] and extract last element []
+// load and find data
 
-// convert to human readeable format and return data [date, price]
+async function fetch_page() {
+	const response = await axios.get("https://www.gw2tp.com/");
+	//console.log(response.data);
+	const $ = cheerio.load(response.data);
+	const price = $(".formatgw2money").text();
+
+	const gold = price.slice(0, 2);
+	const silver = price.slice(2, 4);
+	const date = new Date().toLocaleDateString();
+
+	return [date, { gold, silver }];
+}
+
+fetch_page()
+	.then((res) => console.log(res))
+	.catch((err) => console.log(err));
+
+// return data [date, price]
