e4a7f66685235abf4d867e055891194b8fce1e83
MaccKOT <no@mail.com>
2025-01-10 01:14:55 +0400

    Synced version for more simplicity

--- index.js
+++ index.js
@@ -1,68 +1,60 @@
 import notifier from "node-notifier";
 import * as cheerio from "cheerio";
+import syncFetch from "sync-fetch";
 
 const args = process.argv.slice(2);
 
-// input arguments validation
-if (args.length === 0) {
-	console.error("There's no parameters");
-	process.exit(0);
-}
-try {
-	Number(args[0]).toFixed(2);
-} catch {
+// Input arguments check
+if (args.length === 0 || Number.isNaN(Number(args[0]))) {
 	console.error("Parameter is not a valid number");
-	process.exit(0);
-}
-if (Number.isNaN(Number(args[0]).toFixed(2))) {
-	console.error("Parameter is not a valid number");
-	process.exit(0);
+	console.log("Usage: node ./index.js <Number>");
+	process.exit(1);
 }
 
 const priceToSearch = Number(args[0]).toFixed(2);
 
-// data fetching and scrapping
-async function fetch_gemprice() {
-	const response = await fetch("https://www.gw2tp.com/");
-	const data = await response.text();
+// Price parsing from site
+function fetchGemPriceSync() {
+	const response = syncFetch("https://www.gw2tp.com/");
+	const data = response.text();
 	const $ = cheerio.load(data);
 	const price = $(".formatgw2money").text();
 
 	const gold = price.slice(0, 2);
 	const silver = price.slice(2, 4);
-	if (Number.isNaN(gold) || Number.isNaN(silver))
+
+	if (Number.isNaN(gold) || Number.isNaN(silver)) {
 		throw new Error("Unable to translate price into a number");
+	}
 
 	return Number(`${gold}.${silver}`).toFixed(2);
 }
 
-function desktop_notification(title, message) {
+function desktopNotification(title, message) {
 	notifier.notify({
 		title: title,
 		message: message,
+		wait: false, 
 	});
+
 }
 
-// Main executable
+// Main logic
+try {
+	const gemPrice = fetchGemPriceSync();
 
-fetch_gemprice()
-	.then((gemPrice) => {
-		if (Number.isNaN(Number(priceToSearch))) {
-			console.error("Input data not a number");
-			process.exit(0);
-		}
-		if (gemPrice > priceToSearch) {
-			console.log(
-				`Price checked: ${gemPrice} gold coins per 100 gems. Not today.`,
-			);
-			process.exit(0);
-		}
+	if (gemPrice > priceToSearch) {
+		console.log(
+			`Price checked: ${gemPrice} gold coins per 100 gems. Not today.`,
+		);
+		process.exit(0);
+	}
 
-		// Success
-		const message = `Current price is ${gemPrice} gold for 100 gems.`;
-		console.log(`${message} Time for shopping!`);
-		desktop_notification("Time to buy GEMS 💎 in GW2!", message);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+	// Success
+	const message = `Current price is ${gemPrice} gold for 100 gems.`;
+	console.log(`${message} Time for shopping!`);
+	desktopNotification("Time to buy GEMS 💎 in GW2!", message);
+} catch (err) {
+	console.error("Error:", err.message);
+	process.exit(1);
+}
